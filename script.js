985d0f327ee6ac5a8b71201c3b2f931155981e8f
M Dattis <38086897+MssTake@users.noreply.github.com>
2024-09-01 18:42:26 -0400

    embed and art sample adjustments & visibility

--- script.js
+++ script.js
@@ -20,13 +20,15 @@ function changeTheme(selection) {
 function showSamples(selection) {
   var samples = document.getElementById("change");
     switch (selection) {
-    case "webApplications":
+      case "webApplications":
+      // when you have time, add css that,
+      // on hover:, tells users to click on the embed to open the site in another window
       samples.innerHTML =
-      '<a target="_blank" href="https://msstake.github.io/PantherLoungeDatabase><embed src="https://msstake.github.io/PantherLoungeDatabase"></a>';
+      '<a target="_blank"><embed src="https://msstake.github.io/PantherLoungeDatabase"></a>';
       break;
       case "art":
 samples.innerHTML =
-      '<img src="assets/photos/melting.png" alt="Oct 13, 2022">';
+      '<img src="photos/melting.png" alt="Oct 13, 2022">';
       break;
       default:
         samples.innerHTML = '<p id="change"> </p>'
